67d5280deb90e306b4f9df86f09ae73a68335a1f
mazenalhabeby <a.mazen.dessouky@gmail.com>
2023-03-05 13:01:17 +0100

    edit numFunction

--- utils/numberFormat.ts
+++ utils/numberFormat.ts
@@ -1,11 +1,16 @@
 export function kFormatter(num: any) {
   return Math.abs(num) > 999 && Math.abs(num) < 1000000
-    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + " " + "k"
+    ? //@ts-ignore
+      Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + " " + "k"
     : Math.abs(num) > 999999 && Math.abs(num) < 1000000000
-    ? Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + " " + "M"
+    ? //@ts-ignore
+      Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + " " + "M"
     : Math.abs(num) > 999999999 && Math.abs(num) < 1000000000000
-    ? Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + " " + "B"
+    ? //@ts-ignore
+      Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + " " + "B"
     : Math.abs(num) > 999999999999 && Math.abs(num) < 1000000000000000
-    ? Math.sign(num) * (Math.abs(num) / 1000000000000).toFixed(1) + " " + "T"
-    : Math.sign(num) * Math.abs(num).toFixed(3)
+    ? //@ts-ignore
+      Math.sign(num) * (Math.abs(num) / 1000000000000).toFixed(1) + " " + "T"
+    : //@ts-ignore
+      Math.sign(num) * Math.abs(num).toFixed(3)
 }
