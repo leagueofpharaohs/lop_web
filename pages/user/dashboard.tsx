e2775f6070c0a3147d8eb04e585ddfedee632793
mazenalhabeby <a.mazen.dessouky@gmail.com>
2023-03-09 20:57:15 +0100

    remove dashboard page

--- pages/user/dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import PageWrapper from "@/components/PageWrapper"
-import UserLayout from "@/layouts/UserLayout"
-
-export default function Dashboard() {
-  return <PageWrapper>hello</PageWrapper>
-}
-
-Dashboard.getLayout = UserLayout
